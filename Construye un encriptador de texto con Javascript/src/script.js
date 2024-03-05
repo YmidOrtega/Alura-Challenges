a0fa78b1efc6e90a290ac2524aa9ce7105dd5941
Ymidツ <158487556+YmidOrtega@users.noreply.github.com>
2024-03-04 22:18:29 -0500

    Se termina de crear el encriptador de texto con Javascript y se comienza a dar pequeños retoques ```

--- Construye un encriptador de texto con Javascript/src/script.js
+++ Construye un encriptador de texto con Javascript/src/script.js
@@ -1,72 +1,80 @@
-const enter = 'enter';
-const imes = 'imes';
-const ai = 'ai';
-const ober = 'ober';
-const ufat = 'ufat';
-const resultado = document.getElementById('output');
+const enter = "enter";
+const imes = "imes";
+const ai = "ai";
+const ober = "ober";
+const ufat = "ufat";
+const resultado = document.getElementById("output");
 
-function vowelReplacement (vowelText) {
+function vowelReplacement(vowelText) {
  let vowel = vowelText
-    .replace(/e/g, enter)
-    .replace(/i/g, imes)
-    .replace(/a/g, ai)
-    .replace(/o/g, ober) 
-    .replace(/u/g, ufat);
+  .replace(/e/g, enter)
+  .replace(/i/g, imes)
+  .replace(/a/g, ai)
+  .replace(/o/g, ober)
+  .replace(/u/g, ufat);
  return vowel;
 }
 
-function wordReplacement (wordText) {
-  let word = wordText
-    .replace(/enter/g, 'e')
-    .replace(/imes/g, 'i')
-    .replace(/ai/g, 'a')
-    .replace(/ober/g, 'o')
-    .replace(/ufat/g, 'u');
-  return word;
-
+function wordReplacement(wordText) {
+ let word = wordText
+  .replace(/enter/g, "e")
+  .replace(/imes/g, "i")
+  .replace(/ai/g, "a")
+  .replace(/ober/g, "o")
+  .replace(/ufat/g, "u");
+ return word;
 }
 
 function encriptar() {
-  const text = document.getElementById('input').value;
-  if (text.trim() === '') {
-    alert('Por favor, ingrese un texto para encriptar.');
-    return;
-  }
-  const encryptedText = vowelReplacement(text);
-  resultado.innerText = encryptedText;
-  eraseTextInput();
-  document.getElementById('copy').removeAttribute('hidden');
+ const text = document.getElementById("input").value;
+ if (text.trim() === "") {
+  alert("Por favor, ingrese un texto para encriptar.");
+  return;
+ }
+ const encryptedText = vowelReplacement(text);
+ resultado.innerText = encryptedText;
 
+ document.getElementById("copy").removeAttribute("hidden");
+ document.querySelector(".button-container").style.display = "none";
+
+ eraseTextInput();
+
+ return;
 }
 
-function desencriptar() { 
-  const text = document.getElementById('input').value;
-  if (text.trim() === '') {
-    alert('Por favor, ingrese un texto para encriptar.');
-    return;
-  }
-  const decryptedText = wordReplacement(text);
-  resultado.innerText = decryptedText;
-  eraseTextInput();
-  document.getElementById('copy').removeAttribute('hidden');
-  
+function desencriptar() {
+ const text = document.getElementById("input").value;
+ if (text.trim() === "") {
+  alert("Por favor, ingrese un texto para encriptar.");
+  return;
+ }
+ const decryptedText = wordReplacement(text);
+ resultado.innerText = decryptedText;
+
+ document.getElementById("copy").removeAttribute("hidden");
+ document.querySelector(".button-container").style.display = "none";
+
+ eraseTextInput();
+
+ return;
 }
 
 function copiar() {
-  const copyText = resultado.value;
-  resultado.select();
-  navigator.clipboard.writeText(copyText);
-  window.getSelection().removeAllRanges();
-  console.log(copyText);
-  return;
+ const copyText = resultado.value;
+ resultado.select();
+ navigator.clipboard.writeText(copyText);
+ window.getSelection().removeAllRanges();
+ document.querySelector(".button-container").style.display = "block";
+
+ eraseTextOutput();
 }
 
 function eraseTextInput() {
-  document.getElementById('input').value = '';
-  return;
+ document.getElementById("input").value = "";
+ return;
 }
 
 function eraseTextOutput() {
-  resulto.value = '';
-  return;
+ resultado.innerText = "";
+ return;
 }
